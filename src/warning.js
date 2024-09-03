6622055f6e3253ef07e2105543c39e098f7da51e
Emrys Dai <daishuming4@gmail.com>
2024-09-03 17:08:28 +1000

    home

--- src/warning.js
+++ src/warning.js
@@ -1,16 +1,13 @@
 
 if (window.confirm("The contents in the website may make you feel dizzy and nauseous. Do you wish to preceed?"));
 else {
-    window.open('', '_self', '');
-    window.close();
+    window.location.href = 'about:home';
 }
-
 if (window.screen.width < 1300) {
     if (window.confirm("Your device width is not suitable for this website which may results in bad experiences. Do you wish to preceed?")){
 
     }
     else {
-        window.open('', '_self', '');
-        window.close();
+        window.location.href = 'about:home';
     }
 }
