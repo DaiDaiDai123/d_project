9671b7088fe5ca15cd3d17e0c577cd982fc17aa9
Emrys Dai <daishuming4@gmail.com>
2024-09-03 17:15:46 +1000

    rick

--- src/warning.js
+++ src/warning.js
@@ -1,11 +1,12 @@
 
-if (window.confirm("The contents in the website may make you feel dizzy and nauseous. Do you wish to preceed?"));
+if (window.confirm("The contents in the website may make you feel dizzy and nauseous. Do you wish to preceed?")){
+    
+}
 else {
     window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
 }
 if (window.screen.width < 1300) {
     if (window.confirm("Your device width is not suitable for this website which may results in bad experiences. Do you wish to preceed?")){
-
     }
     else {
         window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
