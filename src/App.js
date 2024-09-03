fab481c2fbccd343560bf0f7a48f303c64477262
Emrys Dai <daishuming4@gmail.com>
2024-09-03 16:18:52 +1000

    try

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
+import SideBar from "./SideBar.js"
+import './yin.css';
 import './App.css';
+import "./warning"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+    <div className = "flex fixed left-0 top-0"><SideBar/></div>
+    </body>
+    
   );
 }
 
